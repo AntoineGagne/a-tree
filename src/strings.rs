eec09b2594495830f346ce53e5f0b7f1e1975ccc
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2024-11-29 14:42:44 -0500

    Add more tests

--- src/strings.rs
+++ src/strings.rs
@@ -37,3 +37,49 @@ impl StringTable {
 
 #[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd, Debug, Hash)]
 pub struct StringId(usize);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const A_KEY: &str = "test";
+    const ANOTHER_KEY: &str = "test_2";
+
+    #[test]
+    fn can_get_a_non_existing_string() {
+        let table = StringTable::new();
+
+        let id = table.get(A_KEY);
+
+        assert_eq!(id, table.get(ANOTHER_KEY));
+    }
+
+    #[test]
+    fn update_the_table_with_the_new_string_when_it_is_not_present() {
+        let mut table = StringTable::new();
+
+        let id = table.get_or_update(A_KEY);
+
+        assert_eq!(id, table.get(A_KEY));
+    }
+
+    #[test]
+    fn return_the_same_id_when_the_same_string_is_given() {
+        let mut table = StringTable::new();
+
+        let id = table.get_or_update(A_KEY);
+
+        assert_eq!(id, table.get_or_update(A_KEY));
+    }
+
+    #[test]
+    fn can_add_multiple_strings() {
+        let mut table = StringTable::new();
+
+        let id = table.get_or_update(A_KEY);
+        let another_id = table.get_or_update(ANOTHER_KEY);
+
+        assert_eq!(id, table.get_or_update(A_KEY));
+        assert_eq!(another_id, table.get_or_update(ANOTHER_KEY));
+    }
+}

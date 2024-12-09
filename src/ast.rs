f3563fa85fca9b15737514c4e42b424d71520e47
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2024-12-09 14:11:20 -0500

    WIP: Start working on insert
    Everything is broken and nothing compiles...

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,5 @@
 use crate::predicates::Predicate;
+use std::hash::{Hash, Hasher};
 
 pub type TreeNode = Box<Node>;
 
@@ -7,5 +8,13 @@ pub enum Node {
     And(TreeNode, TreeNode),
     Or(TreeNode, TreeNode),
     Not(TreeNode),
+    Value(Predicate),
+}
+
+#[derive(Hash, Clone, Eq, PartialEq)]
+pub enum Operator {
+    And,
+    Or,
+    Not,
     Value(Predicate),
 }
--- src/atree.rs
+++ src/atree.rs
@@ -10,14 +10,19 @@ use crate::{
     predicates::Predicate,
     strings::StringTable,
 };
-use std::collections::{hash_map::RandomState, HashMap};
+use bimap::BiMap;
+use slab::Slab;
+use std::borrow::BorrowMut;
+
+type NodeId = usize;
+type ExpressionId = u64;
 
 /// The A-Tree data structure as described by the paper.
 pub struct ATree {
-    nodes: Vec<ATreeNode>,
+    nodes: Slab<ATreeNode>,
     strings: StringTable,
     attributes: AttributeTable,
-    expression_to_node: HashMap<usize, usize>,
+    expression_to_node: BiMap<ExpressionId, NodeId>,
 }
 
 impl ATree {
@@ -39,38 +44,49 @@ impl ATree {
         Ok(Self {
             attributes,
             strings,
-            nodes: vec![],
-            expression_to_node: HashMap::default(),
+            nodes: Slab::new(),
+            expression_to_node: BiMap::new(),
         })
     }
 
     /// Insert an arbitrary boolean expression inside the [`ATree`].
-    pub fn insert<'a, 'tree: 'a>(&'tree mut self, abe: &'a str) -> Result<usize, ATreeError<'a>> {
+    pub fn insert<'a, 'tree: 'a>(&'tree mut self, abe: &'a str) -> Result<NodeId, ATreeError<'a>> {
         let ast = parser::parse(abe, &self.attributes, &mut self.strings)
             .map_err(ATreeError::ParseError)?;
         self.insert_root(ast)
     }
 
-    fn insert_root(&mut self, root: Node) -> Result<usize, ATreeError> {
-        let rnode = match root {
+    fn insert_root(&mut self, root: Node) -> Result<NodeId, ATreeError> {
+        let rnode = match &root {
             Node::And(left, right) | Node::Or(left, right) => {
-                unimplemented!();
+                let nodes = &mut self.nodes;
+                let left_id = insert_node(nodes, &mut self.expression_to_node, &*left)?;
+                let right_id = insert_node(nodes, &mut self.expression_to_node, &*right)?;
+                let mut left_node = nodes[left_id].borrow_mut();
+                let mut right_node = nodes[right_id].borrow_mut();
+                let rnode = RNode {
+                    level: 1 + std::cmp::max(left_node.level(), right_node.level()),
+                    operator: if matches!(root, Node::And(_, _)) {
+                        Operator::And
+                    } else {
+                        Operator::Or
+                    },
+                    children: vec![left_id, right_id],
+                };
+                let expression_id = rnode.id(&self.expression_to_node);
+                rnode
             }
             Node::Not(child) => {
                 unimplemented!();
             }
             Node::Value(value) => RNode {
-                operator: Operator::Value,
-                level: 2,
+                operator: Operator::Value(value.clone()),
+                level: 1,
                 children: vec![],
             },
         };
         let rnode = ATreeNode::RNode(rnode);
         Ok(1)
-    }
-
-    fn insert_node(&mut self, node: &Node) -> Result<usize, ATreeError> {
-        unimplemented!();
     }
 
     /// Create a new [`EventBuilder`] to be able to generate an [`Event`] that will be usable for
@@ -101,6 +117,47 @@ impl ATree {
     }
 }
 
+fn insert_node<'atree, 'a>(
+    nodes: &'atree mut Slab<ATreeNode>,
+    expression_to_node: &'atree mut BiMap<ExpressionId, NodeId>,
+    node: &'a Node,
+) -> Result<NodeId, ATreeError<'a>> {
+    match node {
+        Node::And(left, right) | Node::Or(left, right) => {
+            unimplemented!();
+        }
+        Node::Value(node) => {
+            let expression_id = node.id();
+            let node_id = if let Some(node_id) = expression_to_node.get_by_left(&expression_id) {
+                *node_id
+            } else {
+                let lnode = ATreeNode::LNode(LNode {
+                    level: 1,
+                    parents: vec![],
+                    predicate: node.clone(),
+                });
+                let arena_id = nodes.insert(lnode);
+                // TODO: Revisit this arena_id
+                expression_to_node.insert_no_overwrite(expression_id, arena_id);
+                arena_id
+            };
+            Ok(node_id)
+        }
+        Node::Not(node) => {
+            let child_id = insert_node(nodes, expression_to_node, node)?;
+            let node = nodes[child_id].borrow_mut();
+            let inode = INode {
+                parents: vec![],
+                level: 1 + node.level(),
+                operator: Operator::Not,
+                children: vec![child_id],
+            };
+            let id = 1;
+            Ok(id)
+        }
+    }
+}
+
 enum ATreeNode {
     LNode(LNode),
     INode(INode),
@@ -108,6 +165,15 @@ enum ATreeNode {
 }
 
 impl ATreeNode {
+    fn id(&self, expression_to_node: &BiMap<ExpressionId, NodeId>) -> u64 {
+        match self {
+            ATreeNode::LNode(node) => node.id(),
+            ATreeNode::INode(node) => node.id(expression_to_node),
+            ATreeNode::RNode(node) => node.id(expression_to_node),
+        }
+    }
+
+    #[inline]
     const fn level(&self) -> usize {
         match self {
             ATreeNode::RNode(node) => node.level,
@@ -118,29 +184,89 @@ impl ATreeNode {
 }
 
 struct LNode {
-    parents: Vec<usize>,
+    parents: Vec<NodeId>,
     level: usize,
     predicate: Predicate,
 }
 
+impl LNode {
+    fn id(&self) -> u64 {
+        self.predicate.id()
+    }
+}
+
 struct INode {
-    parents: Vec<usize>,
-    children: Vec<usize>,
+    parents: Vec<NodeId>,
+    children: Vec<NodeId>,
     level: usize,
     operator: Operator,
+}
+
+impl INode {
+    fn id(&self, expression_to_node: &BiMap<ExpressionId, NodeId>) -> u64 {
+        match &self.operator {
+            Operator::And | Operator::Or => {
+                let children_ids = self.children.iter().map(|child| {
+                    expression_to_node.get_by_right(child).expect(&format!(
+                        "no expression ID found for child {child}; this is a bug"
+                    ))
+                });
+                if matches!(self.operator, Operator::And) {
+                    children_ids.product()
+                } else {
+                    children_ids.sum()
+                }
+            }
+            Operator::Not => {
+                let child_id = self
+                    .children
+                    .first()
+                    .expect(&format!("no child ID for 'not' expression; this is a bug"));
+                let child_id = expression_to_node
+                    .get_by_right(child_id)
+                    .expect(&format!("no expression ID for child {child_id}"));
+                !child_id
+            }
+            Operator::Value(_) => {
+                unreachable!("i-node should not be a leaf; this is a bug");
+            }
+        }
+    }
 }
 
 struct RNode {
-    children: Vec<usize>,
+    children: Vec<NodeId>,
     level: usize,
     operator: Operator,
 }
 
-pub enum Operator {
-    And,
-    Or,
-    Not,
-    Value,
+impl RNode {
+    fn id(&self, expression_to_node: &BiMap<ExpressionId, NodeId>) -> u64 {
+        match &self.operator {
+            Operator::And | Operator::Or => {
+                let children_ids = self.children.iter().map(|child| {
+                    expression_to_node.get_by_right(child).expect(&format!(
+                        "no expression ID found for child {child} in r-node; this is a bug"
+                    ))
+                });
+                if matches!(self.operator, Operator::And) {
+                    children_ids.product()
+                } else {
+                    children_ids.sum()
+                }
+            }
+            Operator::Not => {
+                let child_id = self.children.first().expect(&format!(
+                    "no child ID in r-node for 'not' expression; this is a bug"
+                ));
+                let child_id = expression_to_node
+                    .get_by_right(child_id)
+                    .expect(&format!("no expression ID for child {child_id} in r-node"));
+                !child_id
+            }
+            Operator::Value(predicate) => predicate.id(),
+        }
+    }
 }
 
 pub struct Report {

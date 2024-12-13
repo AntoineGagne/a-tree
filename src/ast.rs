9d6dec0447296b36a08b427514b7db0b78ea6a09
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2024-12-13 11:44:49 -0500

    Add a comment for the ID computation

--- src/ast.rs
+++ src/ast.rs
@@ -22,6 +22,16 @@ pub enum Operator {
 impl Node {
     #[inline]
     pub fn id(&self) -> u64 {
+        // TODO: Even though the paper specifies that way of computing the ID, I feel as though
+        // this might yield collisions. For example, if there are some expressions such as
+        // (where A = 3, B = 5, C = 2 and D = 6):
+        //
+        // A ∧ B
+        // (C ∧ D) ∨ A
+        //
+        // Then, given the above expressions, there could be a conflict in the expression IDs.
+        // If this is possible, should this implementation be switch for a commutative hashing
+        // strategy?
         match self {
             Self::And(left, right) => u64::wrapping_mul(left.id(), right.id()),
             Self::Or(left, right) => u64::wrapping_add(left.id(), right.id()),

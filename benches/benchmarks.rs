fd9f0755fa8d8bdb4195b4ffb783a8a277d8ec07
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2024-12-16 14:21:02 -0500

    Add basic benchmark for search
    This is just a first step. A more realist workload should be used
    instead.

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -25,5 +25,38 @@ pub fn insert_expression(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, insert_expression);
+pub fn search(c: &mut Criterion) {
+    let attributes = [
+        AttributeDefinition::integer("exchange_id"),
+        AttributeDefinition::string_list("deal_ids"),
+        AttributeDefinition::integer_list("segment_ids"),
+        AttributeDefinition::string("country"),
+        AttributeDefinition::string("city"),
+    ];
+    let mut atree = ATree::new(&attributes).unwrap();
+    atree.insert(AN_ID, AN_EXPRESSION).unwrap();
+    c.bench_function("insert", |b| {
+        b.iter_batched(
+            || {
+                let mut builder = atree.make_event();
+                builder.with_integer("exchange_id", 5).unwrap();
+                builder
+                    .with_string_list("deal_ids", &["deal-3", "deal-1"])
+                    .unwrap();
+                builder
+                    .with_integer_list("segment_ids", &[3, 4, 5])
+                    .unwrap();
+                builder.with_string("country", "US").unwrap();
+                builder.with_string("city", "AZ").unwrap();
+                builder.build().unwrap()
+            },
+            |event| {
+                let _ = std::hint::black_box(atree.search(event));
+            },
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+criterion_group!(benches, insert_expression, search);
 criterion_main!(benches);

597bab0df612c7aed2ce302a6327578b310be7bd
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2025-01-14 15:00:42 -0500

    Fix broken doctests (#35)

--- examples/search.rs
+++ examples/search.rs
@@ -2,8 +2,10 @@ use a_tree::{ATree, AttributeDefinition};
 use std::collections::HashMap;
 
 const FIRST_EXPRESSION: &str = r#"exchange_id = 1 and deal_ids one of ['deal-1', 'deal-2'] and segment_ids one of [1, 2, 3] and country in ['FR', 'GB']"#;
-const SECOND_EXPRESSION: &str = r#"exchange_id = 1 and deal_ids one of ['deal-1', 'deal-2'] and segment_ids one of [1, 2, 3] and country = 'CA' and city in ['QC'] or country = 'US' and city in ['AZ']"#;
-const THIRD_EXPRESSION: &str = r#"exchange_id = 1 and deal_ids one of ['deal-1', 'deal-2'] and segment_ids one of [1, 2, 3] and country = 'CA' and city in ['QC'] or country = 'US'"#;
+const SECOND_EXPRESSION: &str = r#"(exchange_id = 1 and deal_ids one of ['deal-1', 'deal-2']) and segment_ids one of [1, 2, 3] and ((country = 'CA' and city in ['QC']) or (country = 'US' and city in ['AZ']))"#;
+const THIRD_EXPRESSION: &str = r#"(exchange_id = 1 and deal_ids one of ['deal-1', 'deal-2']) and segment_ids one of [1, 2, 3] and ((country = 'CA' and city in ['QC']) or (country = 'US'))"#;
+const FOURTH_EXPRESSION: &str =
+    r#"exchange_id = 1 and deal_ids one of ['deal-1', 'deal-2'] and segment_ids one of [1, 2, 3]"#;
 
 fn main() {
     // Create the A-Tree
@@ -21,6 +23,7 @@ fn main() {
         (1, FIRST_EXPRESSION),
         (2, SECOND_EXPRESSION),
         (3, THIRD_EXPRESSION),
+        (4, FOURTH_EXPRESSION),
     ];
     let mappings: HashMap<u64, &str> = HashMap::from_iter(expressions_by_ids);
     for (id, expression) in &expressions_by_ids {
@@ -29,7 +32,7 @@ fn main() {
 
     // Create the matching event
     let mut builder = atree.make_event();
-    builder.with_integer("exchange_id", 5).unwrap();
+    builder.with_integer("exchange_id", 1).unwrap();
     builder
         .with_string_list("deal_ids", &["deal-3", "deal-1"])
         .unwrap();
@@ -41,7 +44,7 @@ fn main() {
     let event = builder.build().unwrap();
 
     // Search inside the A-Tree for matching events
-    let report = atree.search(event).unwrap();
+    let report = atree.search(&event).unwrap();
     report.matches().iter().for_each(|id| {
         println!(r#"Found ID: {id}, Expression: "{}""#, mappings[id]);
     });
